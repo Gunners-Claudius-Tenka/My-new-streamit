4e871b51ded3b645b3b64f08366fb636a5e746c0
Gunners-Claudius-Tenka <1902460329@qq.com>
2024-11-08 19:59:10 +0800

    change

--- test_se.py
+++ test_se.py
@@ -1,9 +1,11 @@
 #先写一下这个计划吧
-#1.摄像头 2.读取文件(dcm) 3.生成docx文件(不确定能不能行) 4.图像增强 5.考虑一下布局美观
+#(1.摄像头) 2.读取文件(dcm) (3.生成docx文件(不确定能不能行)) 4.图像增强 5.考虑一下布局美观
 import streamlit as st
 from docx import Document
 from docx.shared import Inches
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 from PIL import Image
+from datetime import datetime
 import io
 import tempfile
 import os
@@ -17,13 +19,28 @@ if picture:
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
     # 保存图像到临时文件
     image.save(temp_file.name, format='PNG')
+    temp_file.close()  # 关闭文件
+
+    # 获取时间
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # 创建Word文档
     doc = Document()
-    doc.add_picture(temp_file.name, width=Inches(4))  # 设置图片宽度
+    heading = doc.add_heading('这是一个关于照片的测试', level=1)
+    heading.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    p = doc.add_paragraph()
+    p.add_run('当前时间：').bold = True  # 将“当前时间：”设置为粗体
+    p.add_run(current_time)  # 添加当前时间
+    p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    # 添加一个居中的段落用于图片
+    p = doc.add_paragraph()
+    p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+    p.add_run().add_picture(temp_file.name, width=Inches(4))  # 设置图片宽度并居中
 
     # 保存Word文档到临时文件
     temp_doc_file = tempfile.NamedTemporaryFile(delete=False, suffix='.docx')
     doc.save(temp_doc_file.name)
+    temp_doc_file.close()  # 关闭文件
 
     # 下载按钮
     st.download_button(
